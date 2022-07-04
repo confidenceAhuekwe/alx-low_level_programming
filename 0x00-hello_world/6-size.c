170b33d3fe546efff19e10444389c789bd1c882c
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-04 11:26:57 -0700

    Task 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	printf("%s%lu%s\n", "Size of a char: ", sizeof(char), " byte(s)");
+	printf("%s%lu%s\n", "Size of an int: ", sizeof(int), " byte(s)");
+	printf("%s%lu%s\n", "Size of a long int: ", sizeof(long), " byte(s)");
+	printf("%s%lu%s\n", "Size of a long long int: ",
+			sizeof(long long), " byte(s)");
+	printf("%s%lu%s\n", "Size of a float: ", sizeof(float), " byte(s)");
+	return (0);
+}
