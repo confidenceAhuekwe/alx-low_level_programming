704c0be4d2d3786f1b41cbd4502cc205e6dbda53
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-04 10:15:00 -0700

    Task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print Programming is like building a multilingual puzzle
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
