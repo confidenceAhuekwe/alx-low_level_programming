ea9cf6adcbfdcda9684ccd6a6a2efc259fb6a323
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-05 08:10:35 -0700

    Task 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * main - output all single digit numbers of base 10 staring from 0
- * REturn: 0 if successful
+ * Return: 0 if successful
  */
 
 int main(void)
@@ -10,6 +10,6 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d\n", var);
+		printf("%d\n", num);
 	return (0);
 }
