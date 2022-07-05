e7cc7004e53979b9be50cf77704efb6a904dc322
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-05 08:06:08 -0700

    Task A 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - output all single digit numbers of base 10 staring from 0
+ * REturn: 0 if successful
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d\n", var);
+	return (0);
+}
