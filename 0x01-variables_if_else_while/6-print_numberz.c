d03d6c54761933c2aaff213d9f354e5bb95945b8
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-05 08:25:19 -0700

    Task 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - output all single digit numbers of base 10 using putchar
+ * Return: on success 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		puchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
