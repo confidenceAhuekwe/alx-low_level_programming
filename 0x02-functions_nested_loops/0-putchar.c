12ac3589eea7cce0e95fb58082b65264f60afe3f
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-06 15:43:05 -0700

    Task 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,26 @@
-#include <stdio>
+#include "main.h"
 
 /**
- * main - prints _putchar
- * Return: on success 0
+ * main - program that prints _putchar
+ *
+ * created by: confidenceAhuekwe
+ * cc: 6 July, 2022
+ *
+ * Return: On success 0
  */
 
 int main(void)
 {
-	printf("_putchar");
+	char var[] = "_putchar";
+	int m;
+	/*length of the array var*/
+	int lenVar = sizeof(var) / sizeof(var[0]);
 
+	for (m = 0; m < lenVar - 1; m++)
+	{
+		_putchar(var[m]);
+	}
+
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My README file for C - Functions, nested loops
