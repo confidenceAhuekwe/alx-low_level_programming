39055e885ba2031dd4b619266a94199b795e6dd1
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-06 21:16:16 -0700

    Task 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 	long int n1 = 1, result, n2 = 2, nLast = 50;
-	int m;
+	int ii;
 
 	printf("%lu, %lu, ", n1, n2);
-	for (m = 2; m < nLast; m++)
+	for (ii = 2; ii < nLast; ii++)
 	{
 		result = n1 + n2;
 		n1 = n2;
 		n2 = result;
 		printf("%lu", result);
-		if (m != nLast - 1)
+		if (ii != nLast - 1)
 			printf(",");
 	}
 	printf("\n");
