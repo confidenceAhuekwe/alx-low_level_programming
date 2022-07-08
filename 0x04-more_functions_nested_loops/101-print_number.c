0782f5702572ab9215ff4574a4f91a48f1801a6c
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-07-08 04:16:25 -0700

    Task 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * print_number - prints an integer.
+ * @n: argument of the Function of an integer type
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
