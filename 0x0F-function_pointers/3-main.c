2213db2f4500321fd4ec26a45391055bfda6c597
confidenceAhuekwe <confidenceahuekwe@gmail.com>
2022-08-04 01:30:23 -0700

    Tasks on 0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - get_op_func has operators correlated with
+ * func signs and funcs from op_func
+ * if not 4 arguments, return Error & exit 98
+ * if op is null, return Error & exit 99
+ * if div or mod 0, return Error & exit 100
+ * run calc, input one, operator, input two = pointer res to get_op
+ * @argc: argumens
+ * @argv: double pointer to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int one, two, ans;
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/*8
+ * op_add - function to add
+ * @a: first integer
+ * @b: second int
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to subtract
+ * @a: first integer
+ * @b: second integer
+ * Return: difference between a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies teo variables
+ * @a: first variable of type integer
+ * @b: second variable of type integer
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integer variables
+ * @a: first integer variable
+ * @b: second integer variable
+ *
+ * Return: return the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder of the division of two integers
+ * @a: first integer variable
+ * @b: second integer variable
+ *
+ * Return: the remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
